0b7a6977351f1daf1ea3b115b4ed291e13d27773
Matt McCormick <matt.mccormick@kitware.com>
2016-05-26 16:35:41 -0400

    ENH: Add Docker and CircleCI testing configuration.

---
+++ test/Docker/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is a script to build the modules and run the test suite in the base
+# Docker container.
+
+set -x
+set -o
+
+cd /usr/src/ITKPerformanceBenchmarks
+branch=$(git rev-parse --abbrev-ref HEAD)
+date=$(date +%F_%H_%M_%S)
+sha=$(git rev-parse --short HEAD)
+
+cd /usr/src/ITKPerformanceBenchmarks-build
+
+cmake \
+  -G Ninja \
+  -DITK_DIR:PATH=/usr/src/ITK-build \
+  -DCMAKE_BUILD_TYPE:STRING=Release \
+  -DBUILDNAME:STRING=External-PerformanceBenchmarks-${branch}-${date}-${sha} \
+    /usr/src/ITKPerformanceBenchmarks
+ctest -VV -D Experimental
