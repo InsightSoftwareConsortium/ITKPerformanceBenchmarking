b5043bdf16aea4ab6aa37939c7fbbfb780f45b04
Matt McCormick <matt.mccormick@kitware.com>
2016-06-06 17:34:01 -0400

    ENH: Add Watershed benchmark

---
+++ examples/Segmentation/WatershedBenchmark.cxx
@@ -0,0 +1,118 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkCurvatureFlowImageFilter.h"
+#include "itkWatershedImageFilter.h"
+#include "itkGradientMagnitudeRecursiveGaussianImageFilter.h"
+#include "itkRelabelComponentImageFilter.h"
+
+#include "itkHighPriorityRealTimeProbesCollector.h"
+
+#include <fstream>
+
+
+int main( int argc, char * argv[] )
+{
+  if( argc < 5 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << " timingsFile threads inputImageFile outputImageFile" << std::endl;
+    return EXIT_FAILURE;
+    }
+  const char * timingsFileName = argv[1];
+  int threads = atoi( argv[2] );
+  const char * inputImageFileName = argv[3];
+  const char * outputImageFileName = argv[4];
+
+  if( threads > 0 )
+    {
+    itk::MultiThreader::SetGlobalDefaultNumberOfThreads( threads );
+    }
+
+  const unsigned int Dimension = 3;
+  typedef float  PixelType;
+
+  typedef itk::Image< PixelType, 3 > ImageType;
+
+  typedef itk::ImageFileReader< ImageType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( inputImageFileName );
+  try
+    {
+    reader->UpdateLargestPossibleRegion();
+    }
+  catch( itk::ExceptionObject & error )
+    {
+    std::cerr << "Error: " << error << std::endl;
+    return EXIT_FAILURE;
+    }
+  ImageType::Pointer inputImage = reader->GetOutput();
+  inputImage->DisconnectPipeline();
+
+  typedef itk::CurvatureFlowImageFilter< ImageType, ImageType > SmoothingFilterType;
+  SmoothingFilterType::Pointer smoothingFilter = SmoothingFilterType::New();
+  smoothingFilter->SetInput( inputImage );
+  smoothingFilter->SetNumberOfIterations( 2 );
+  smoothingFilter->SetTimeStep( 0.05 );
+
+  typedef itk::GradientMagnitudeRecursiveGaussianImageFilter< ImageType, ImageType > GradientMagnitudeFilterType;
+  GradientMagnitudeFilterType::Pointer gradientMagnitudeFilter = GradientMagnitudeFilterType::New();
+  gradientMagnitudeFilter->SetInput( smoothingFilter->GetOutput() );
+  gradientMagnitudeFilter->SetSigma( 5.0 );
+
+  typedef itk::WatershedImageFilter< ImageType > WatershedFilterType;
+  WatershedFilterType::Pointer watershedFilter = WatershedFilterType::New();
+  watershedFilter->SetInput( gradientMagnitudeFilter->GetOutput() );
+  watershedFilter->SetThreshold( 0.0001 );
+  watershedFilter->SetLevel( 0.3 );
+
+  typedef WatershedFilterType::OutputImageType LabelImageType;
+  typedef itk::RelabelComponentImageFilter< LabelImageType, LabelImageType > RelabelFilterType;
+  RelabelFilterType::Pointer relabelFilter = RelabelFilterType::New();
+  relabelFilter->SetInput( watershedFilter->GetOutput() );
+  relabelFilter->SetMinimumObjectSize( 200 );
+
+  itk::HighPriorityRealTimeProbesCollector collector;
+  const unsigned int numberOfIterations = 3;
+  for( unsigned int ii = 0; ii < numberOfIterations; ++ii )
+    {
+    inputImage->Modified();
+    collector.Start("Watershed");
+    relabelFilter->UpdateLargestPossibleRegion();
+    collector.Stop("Watershed");
+    }
+  bool printSystemInfo = true;
+  bool printReportHead = true;
+  bool useTabs = false;
+  collector.Report( std::cout, printSystemInfo, printReportHead, useTabs );
+
+  std::ofstream timingsFile( timingsFileName, std::ios::out );
+  printSystemInfo = false;
+  useTabs = true;
+  collector.ExpandedReport( timingsFile, printSystemInfo, printReportHead, useTabs );
+
+  typedef itk::ImageFileWriter< LabelImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( outputImageFileName );
+  writer->SetInput( relabelFilter->GetOutput() );
+  writer->Update();
+
+  return EXIT_SUCCESS;
+}
