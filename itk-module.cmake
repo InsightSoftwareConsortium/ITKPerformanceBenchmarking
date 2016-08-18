5f599a0732fdbea319bed81b4e2c0951cd6378fd
Matt McCormick <matt.mccormick@kitware.com>
2016-08-18 09:50:37 -0400

    DOC: Improve module documentation

--- itk-module.cmake
+++ itk-module.cmake
@@ -1,4 +1,17 @@
-set(DOCUMENTATION "ITK Performance Benchmarks")
+set(DOCUMENTATION "New classes increase operating system process priority to
+minimize the impact of other processes running on the system.
+
+These classes are used by a used by a suite of example ITK benchmarks to
+quantify toolkit performance.
+
+For more information, see::
+
+  McCormick M., Kang H.J., Barre S.
+  Performance Benchmarking the Insight Toolkit
+  The Insight Journal. January-December. 2016.
+  http://hdl.handle.net/10380/3557
+  http://insight-journal.org/browse/publication/972
+")
 
 itk_module(PerformanceBenchmarking
   ENABLE_SHARED
