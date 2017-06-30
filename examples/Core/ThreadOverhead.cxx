c94e98cee6fe76243a8d5a904cb570872c971e33
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-06-30 14:21:55 -0400

    ENH: Add benchmark which estimates the overhead of additional thread

---
+++ examples/Core/ThreadOverhead.cxx
@@ -0,0 +1,144 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkImage.h"
+#include "itkUnaryFunctorImageFilter.h"
+#include "itkTimeProbe.h"
+#include "itkHighPriorityRealTimeProbe.h"
+
+#include <sstream>
+
+
+// This benchmark estimate the overhead for using an additional thread
+// in an ImageFilter a.k.a the time it takes to "spawn" a thread.
+//
+// The overhead for spawning threads is computed by measuring the
+// time it takes an Functor Filter to run with 1 thread on 1 pixel,
+// and the time it takes to run with N threads on N pixels. Each
+// thread does the one pixel trivial operation. The difference in
+// execution time is considered the overhead for spawning the
+// threads. Dividing by the number of additional threads gives us the
+// overhead cost of “spawning” or dispatching a single thread.
+
+//typedef itk::TimeProbe    ProbeType;
+
+typedef itk::HighPriorityRealTimeProbe    ProbeType;
+
+namespace
+{
+template< typename TInput, typename TOutput >
+class Op
+{
+public:
+  Op() {}
+  virtual ~Op() {}
+  bool operator!=(const Op &) const
+  {
+    return false;
+  }
+
+  bool operator==(const Op & other) const
+  {
+    return !( *this != other );
+  }
+
+  inline TOutput operator()(const TInput & A) const
+  {
+    return static_cast< TOutput >( A + 1);
+  }
+};
+}
+
+ProbeType time_it(unsigned int threads, unsigned int iterations, bool realtime = true)
+{
+
+  const unsigned int Dimension = 1;
+  typedef float PixelType;
+
+  typedef itk::Image<PixelType,Dimension> ImageType;
+
+  ImageType::Pointer image = ImageType::New();
+
+  ImageType::SizeType imageSize = { threads };
+  image->SetRegions(ImageType::RegionType(imageSize));
+  image->Allocate();
+  image->FillBuffer(0);
+
+
+  typedef itk::UnaryFunctorImageFilter< ImageType,
+                                        ImageType,
+                                        Op<PixelType,PixelType > > FilterType;
+
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput(image);
+  filter->SetNumberOfThreads( threads );
+
+  // execute one time out of the loop to allocate memory
+  filter->UpdateLargestPossibleRegion();
+
+  std::ostringstream ss;
+  ss << "FilterWithThreads-" << threads;
+
+  std::string name = ss.str();
+
+  ProbeType timer;
+  timer.SetNameOfProbe(name.c_str());
+  for( int ii = 0; ii < iterations; ++ii )
+    {
+    image->Modified();
+    timer.Start();
+    filter->UpdateLargestPossibleRegion();
+    timer.Stop();
+    }
+  static bool printReportHead = true;
+  bool printSystemInfo = false;
+  bool useTabs = false;
+  timer.Report( std::cout, printSystemInfo, printReportHead, useTabs );
+  printReportHead = false;
+
+  return timer;
+}
+
+
+int main( int argc, char * argv[] )
+{
+  if( argc > 3 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << "[iterations [threads]]" << std::endl;
+    return EXIT_FAILURE;
+    }
+  const int iterations = (argc>1) ? atoi( argv[1] ): 1000;
+  const int threads = (argc>2) ? atoi( argv[2] ) : itk::MultiThreader::GetGlobalDefaultNumberOfThreads();
+
+  if (threads == 1)
+    {
+    std::cout << "Unable to estimate the cost with only one thread!" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  ProbeType t1 = time_it(1,iterations);
+
+  ProbeType t2 = time_it(threads,iterations);
+
+  double cost = (t2.GetMinimum() - t1.GetMinimum())/(threads-1.0);
+
+  std::cout << "Estimated overhead cost per thread: " << cost * 1e6 << " micro-seconds" << std::endl;
+
+  return EXIT_SUCCESS;
+}
