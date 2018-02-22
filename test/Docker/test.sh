83af7132ebd3e983f7b4b14dbc185822493d3e59
Matt McCormick <matt.mccormick@kitware.com>
2018-02-22 10:41:07 -0500

    ENH: Use CTest script for Docker image dashboard submission
    Adds the Git revision to CTest Update field.

--- test/Docker/test.sh
+++ test/Docker/test.sh
@@ -12,19 +12,42 @@ date=$(date +%F_%H_%M_%S)
 sha=$(git rev-parse --short HEAD)
 
 cd /ITKPerformanceBenchmarking-build
-cmake \
-  -G Ninja \
-  -DITK_DIR:PATH=/ITK-build \
-  -DCMAKE_BUILD_TYPE:STRING=Release \
-  -DBUILDNAME:STRING=External-PerformanceBenchmarking-${branch}-${date}-${sha} \
-    /ITKPerformanceBenchmarking
-ctest -VV -D Experimental
+curl -L https://raw.githubusercontent.com/InsightSoftwareConsortium/ITK/dashboard/itk_common.cmake -O
+cat > dashboard.cmake << EOF
+set(CTEST_SITE "ITKPerformanceBenchmarkingDockerContainer")
+set(CTEST_BUILD_NAME "External-PerformanceBenchmarking-${branch}-${date}-${sha}")
+set(CTEST_BUILD_CONFIGURATION "Release")
+set(CTEST_CMAKE_GENERATOR "Ninja")
+set(CTEST_BUILD_FLAGS: "-j5")
+set(CTEST_SOURCE_DIRECTORY /ITKPerformanceBenchmarking)
+set(CTEST_BINARY_DIRECTORY /ITKPerformanceBenchmarking-build)
+set(dashboard_model Experimental)
+set(dashboard_no_clean 1)
+set(dashboard_no_update 1)
+set(dashboard_cache "
+ITK_DIR:PATH=/ITK-build
+BUILD_TESTING:BOOL=ON
+")
+include(\${CTEST_SCRIPT_DIRECTORY}/itk_common.cmake)
+EOF
+ctest -VV -S dashboard.cmake
 
 cd /ITKBenchmarks-build
-cmake \
-  -G Ninja \
-  -DITK_DIR:PATH=/ITK-build \
-  -DCMAKE_BUILD_TYPE:STRING=Release \
-    /ITKPerformanceBenchmarking/examples/
-ninja
-ctest -V
+curl -L https://raw.githubusercontent.com/InsightSoftwareConsortium/ITK/dashboard/itk_common.cmake -O
+cat > dashboard.cmake << EOF
+set(CTEST_SITE "ITKPerformanceBenchmarkingDockerContainer")
+set(CTEST_BUILD_NAME "External-PerformanceBenchmarking-Benchmarks-${branch}-${date}-${sha}")
+set(CTEST_BUILD_CONFIGURATION "Release")
+set(CTEST_CMAKE_GENERATOR "Ninja")
+set(CTEST_BUILD_FLAGS: "-j5")
+set(CTEST_SOURCE_DIRECTORY /ITKPerformanceBenchmarking/examples)
+set(CTEST_BINARY_DIRECTORY /ITKBenchmarks-build)
+set(dashboard_model Experimental)
+set(dashboard_no_clean 1)
+set(dashboard_no_update 1)
+set(dashboard_cache "
+ITK_DIR:PATH=/ITK-build
+")
+include(\${CTEST_SCRIPT_DIRECTORY}/itk_common.cmake)
+EOF
+ctest -VV -S dashboard.cmake
