82c6bf66821735fb97ba9e699fc4213310b037e6
Matt McCormick <matt.mccormick@kitware.com>
2018-05-13 21:16:09 -0400

    COMP: Fix PerformanceBenchmarkingUtilities export specification
    Addresses:
    
      examples/Core/CMakeFiles/ThreadOverheadBenchmark.dir/ThreadOverhead.cxx.o:
      In function `main':
      ThreadOverhead.cxx:(.text.startup+0x79): undefined reference to
      `ReplaceOccurrence(std::__cxx11::basic_string<char, std::char_traits<char>,
      std::allocator<char> >, std::__cxx11::basic_string<char,
      std::char_traits<char>, std::allocator<char> > const&&,
      std::__cxx11::basic_string<char, std::char_traits<char>,
      std::allocator<char> > const&&)'

--- include/PerformanceBenchmarkingUtilities.h
+++ include/PerformanceBenchmarkingUtilities.h
@@ -2,23 +2,34 @@
 // Created by Hans J. Johnson on 2/19/18.
 //
 
-#ifndef PERFORMANCEBENCHMARKING_PERFORMANCEBENCHMARKINGUTILITIES_H
-#define PERFORMANCEBENCHMARKING_PERFORMANCEBENCHMARKINGUTILITIES_H
+#ifndef PerformanceBenchmarkingUtilities_h
+#define PerformanceBenchmarkingUtilities_h
+
+#include "PerformanceBenchmarkingExport.h"
 
 #include "jsonxx.h"
 #include <ctime> //TODO:  Move to utiliites
 #include "itkHighPriorityRealTimeProbesCollector.h"
 
-extern std::string PerfDateStamp();
+PerformanceBenchmarking_EXPORT
+std::string
+PerfDateStamp();
 
-extern std::string ReplaceOccurrence( std::string str,
-                                     const  std::string && findvalue,
-                                     const  std::string && replacevalue);
+PerformanceBenchmarking_EXPORT
+std::string
+ReplaceOccurrence( std::string str,
+                   const  std::string && findvalue,
+                   const  std::string && replacevalue );
 
-extern std::string DecorateWithBuildInformation( std::string  inputJson);
+PerformanceBenchmarking_EXPORT
+std::string
+DecorateWithBuildInformation( std::string inputJson);
 
-extern void WriteExpandedReport(const std::string &timingsFileName, itk::HighPriorityRealTimeProbesCollector &collector,
-                                bool printSystemInfo, bool printReportHead, bool useTabs);
-
-
-#endif //PERFORMANCEBENCHMARKING_PERFORMANCEBENCHMARKINGUTILITIES_H
+PerformanceBenchmarking_EXPORT
+void
+WriteExpandedReport( const std::string &timingsFileName,
+                     itk::HighPriorityRealTimeProbesCollector &collector,
+                     bool printSystemInfo,
+                     bool printReportHead,
+                     bool useTabs );
+#endif
